f56606c30791ab48b0493f4ef0e51e187de2d541
sourpunchlover <sndgpdesigns@gmail.com>
2026-04-24 09:55:10 -0400

    Added public static void homeScreen & put while loop inside. Added homeScreen() and passed in scanner under public static void main.

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -8,28 +8,38 @@ public class App {
 
         System.out.println("Welcome to the online store!");
 
-        System.out.println("Select from the following options: ");
-        System.out.println("\t) Display Products");
-        System.out.println("\t) Display Cart");
-        System.out.println("\t0) Exit");
-        System.out.println("Enter you selection: ");
-        int userOption = scanner.nextInt();
+        homeScreen(scanner);
 
-        switch (userOption){
-            case 1:
-                displayProducts();
-               break;
-            case 2:
-                displayCart();
-               break;
-            case 0:
-                //exit system. 0 is a successful exit
-                System.out.println("Thanks for visiting!)");
-                System.exit(0);
-                break;
-            default:
-                System.out.println("Incorrect option entered");
+    }
+    public static void homeScreen(Scanner scanner) {
+        while (true) {
+            System.out.println("Select from the following options: ");
+            System.out.println("\t1) Display Products");
+            System.out.println("\t2) Display Cart");
+            System.out.println("\t0) Exit");
+            System.out.println("Enter your selection: ");
+            int userOption = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (userOption) {
+                case 1:
+                    //displayProducts();
+                    System.out.println("\n\n");
+                    break;
+                case 2:
+                    //displayCart();
+                    System.out.println("\n\n");
+                    break;
+                case 0:
+                    //exit system. 0 is a successful exit
+                    System.out.println("Thanks for visiting \uD83D\uDE0A!)");
+                    scanner.close();
+                    System.exit(0);
+                    break;
+                default:
+            }        System.out.println("Incorrect option entered (press Enter to continue)");
+            scanner.nextLine();
+            System.out.println("\n\n");
         }
-
     }
 }
--- src/main/java/com/pluralsight/Products.java
+++ src/main/java/com/pluralsight/Products.java
@@ -1,7 +1,7 @@
 package com.pluralsight;
 
 public class Products {
-    private String sku , productName , department:
+    private String sku , productName , department;
     private double price;
 
     public Products(String sku, String productName, String department, double price) {
