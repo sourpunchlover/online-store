e952819099c543d85a89132ecfab8792717ace5b
sourpunchlover <sndgpdesigns@gmail.com>
2026-04-24 09:25:43 -0400

    Added Scanner & and user input statements.

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,4 +1,19 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the online store!");
+
+        System.out.println("Select from the following options: ");
+        System.out.println("\t) Display Products");
+        System.out.println("\t) Display Cart");
+        System.out.println("\t0) Exit");
+        System.out.println("Enter you selection: ");
+        int userOption = scanner.nextInt();
+
+    }
 }
--- src/main/java/com/pluralsight/Products.java
+++ src/main/java/com/pluralsight/Products.java
@@ -1,4 +1,29 @@
 package com.pluralsight;
 
 public class Products {
+    private String sku , productName , department:
+    private double price;
+
+    public Products(String sku, String productName, String department, double price) {
+        this.sku = sku;
+        this.productName = productName;
+        this.department = department;
+        this.price = price;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
 }
